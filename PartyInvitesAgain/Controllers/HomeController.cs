3004961c7563fc93c8752c4bf2ab27cd067733e0
jeanschwalenberg <jeanschwalenberg@gmail.com>
2015-12-28 11:16:20 -0700

    Initial Commit (very late)

---
+++ PartyInvitesAgain/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvitesAgain.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    /*public string Index()
+        {
+            return "Can return string directly into View!";
+        }*/
+        // GET: Home
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning," : "Good afternoon,";
+            return View();
+        }
+    }
+}
---
+++ PartyInvitesAgain/Models/GuestResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvitesAgain.Models
+{
+    public class GuestResponse
+    {
+
+    }
+}
